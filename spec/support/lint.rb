6080558ac9c7c3c59c547fb6ea8bc42b34285af4
Taylor Thurlow <taylorthurlow@me.com>
2020-10-30 14:15:43 -0700

    Refactor/rework specs for interactor -> interaktor changes

--- spec/support/lint.rb
+++ spec/support/lint.rb
@@ -9,74 +9,235 @@ shared_examples "lint" do
   end
 
   describe ".call" do
-    let(:context) { instance_double(Interaktor::Context) }
     let(:instance) { instance_double(interaktor) }
 
-    it "calls an instance with the given context" do
-      instance.instance_variable_set(:@context, context)
-      expect(interaktor).to receive(:new).once.with(foo: "bar").and_return(instance)
-      expect(instance).to receive(:run).once.with(no_args)
+    it "calls an instance" do
+      expect(interaktor).to receive(:new).once.with({}).and_call_original
 
-      resulting_context = interaktor.call(foo: "bar")
+      result = interaktor.call
 
-      expect(resulting_context).to eq(context)
+      expect(result.success?).to be true
     end
 
-    it "provides a blank context if none is given" do
-      instance.instance_variable_set(:@context, context)
-      expect(interaktor).to receive(:new).once.with({}).and_return(instance)
-      expect(instance).to receive(:run).once.with(no_args)
+    it "fails when an unknown attribute is provided" do
+      expect { interaktor.call(baz: "wadus") }.to raise_error(RuntimeError, /not recognized/)
+    end
 
-      resulting_context = interaktor.call({})
+    context "when calling an instance with a required attribute" do
+      it "succeeds when the attribute is provided" do
+        interaktor.class_eval { required :bar }
 
-      expect(resulting_context).to eq(context)
+        attributes = { bar: "baz" }
+        expect(interaktor).to receive(:new).once.with(attributes).and_call_original
+
+        result = interaktor.call(attributes)
+
+        expect(result.success?).to be true
+        attributes.each { |attr, value| expect(result.send(attr)).to eq value }
+      end
+
+      it "fails when the attribute is not provided" do
+        interaktor.class_eval { required :bar }
+
+        expect { interaktor.call }.to raise_error(RuntimeError, /not provided/)
+      end
+    end
+
+    context "when calling an instance with an optional attribute" do
+      it "succeeds when the attribute is provided" do
+        interaktor.class_eval { optional :bar }
+
+        attributes = { bar: "baz" }
+        expect(interaktor).to receive(:new).once.with(attributes).and_call_original
+
+        result = interaktor.call(attributes)
+
+        expect(result.success?).to be true
+        attributes.each { |attr, value| expect(result.send(attr)).to eq value }
+      end
+
+      it "succeeds when the attribute is not provided" do
+        interaktor.class_eval { optional :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        result = interaktor.call
+
+        expect(result.success?).to be true
+      end
+    end
+
+    context "when early-exiting an interaktor with a success" do
+      it "succeeds when the correct attributes are provided" do
+        interaktor.class_eval { success :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          success!(bar: "baz")
+        end
+
+        result = interaktor.call
+
+        expect(result.success?).to be true
+        expect(result.bar).to eq "baz"
+      end
+
+      it "raises an exception when the correct attributes are not provided" do
+        interaktor.class_eval { success :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          success!({})
+        end
+
+        expect { interaktor.call }.to raise_error(RuntimeError, /Missing success attrs/)
+      end
+    end
+
+    context "when exiting an interaktor with a failure" do
+      it "fails successfully when the correct attributes are provided" do
+        interaktor.class_eval { failure :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          fail!(bar: "baz")
+        end
+
+        result = interaktor.call
+
+        expect(result.success?).to be false
+        expect(result.bar).to eq "baz"
+      end
+
+      it "raises an exception when the correct attributes are not provided" do
+        interaktor.class_eval { failure :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          fail!({})
+        end
+
+        expect { interaktor.call }.to raise_error(RuntimeError, /Missing failure attrs/)
+      end
     end
   end
 
   describe ".call!" do
-    let(:context) { instance_double(Interaktor::Context) }
     let(:instance) { instance_double(interaktor) }
 
-    it "calls an instance with the given context" do
-      instance.instance_variable_set(:@context, context)
-      expect(interaktor).to receive(:new).once.with(foo: "bar").and_return(instance)
-      expect(instance).to receive(:run!).once.with(no_args)
+    it "calls an instance" do
+      expect(interaktor).to receive(:new).once.with({}).and_call_original
 
-      resulting_context = interaktor.call!(foo: "bar")
+      result = interaktor.call!
 
-      expect(resulting_context).to eq(context)
+      expect(result.success?).to be true
     end
 
-    it "provides a blank context if none is given" do
-      instance.instance_variable_set(:@context, context)
-      expect(interaktor).to receive(:new).once.with({}).and_return(instance)
-      expect(instance).to receive(:run!).once.with(no_args)
-
-      resulting_context = interaktor.call!({})
-
-      expect(resulting_context).to eq(context)
-    end
-  end
-
-  describe ".new" do
-    let(:context) { instance_double(Interaktor::Context) }
-
-    it "initializes a context" do
-      expect(Interaktor::Context).to receive(:build).once.with(foo: "bar").and_return(context)
-
-      instance = interaktor.new(foo: "bar")
-
-      expect(instance).to be_an(interaktor)
-      expect(instance.instance_variable_get(:@context)).to eq(context)
+    it "fails when an unknown attribute is provided" do
+      expect { interaktor.call!(baz: "wadus") }.to raise_error(RuntimeError, /not recognized/)
     end
 
-    it "initializes a blank context if none is given" do
-      expect(Interaktor::Context).to receive(:build).once.with({}).and_return(context)
+    context "when calling an instance with a required attribute" do
+      it "succeeds when the attribute is provided" do
+        interaktor.class_eval { required :bar }
 
-      instance = interaktor.new
+        attributes = { bar: "baz" }
+        expect(interaktor).to receive(:new).once.with(attributes).and_call_original
 
-      expect(instance).to be_a(interaktor)
-      expect(instance.instance_variable_get(:@context)).to eq(context)
+        result = interaktor.call!(attributes)
+
+        expect(result.success?).to be true
+        attributes.each { |attr, value| expect(result.send(attr)).to eq value }
+      end
+
+      it "fails when the attribute is not provided" do
+        interaktor.class_eval { required :bar }
+
+        expect { interaktor.call! }.to raise_error(RuntimeError, /not provided/)
+      end
+    end
+
+    context "when calling an instance with an optional attribute" do
+      it "succeeds when the attribute is provided" do
+        interaktor.class_eval { optional :bar }
+
+        attributes = { bar: "baz" }
+        expect(interaktor).to receive(:new).once.with(attributes).and_call_original
+
+        result = interaktor.call!(attributes)
+
+        expect(result.success?).to be true
+        attributes.each { |attr, value| expect(result.send(attr)).to eq value }
+      end
+
+      it "succeeds when the attribute is not provided" do
+        interaktor.class_eval { optional :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        result = interaktor.call!
+
+        expect(result.success?).to be true
+      end
+    end
+
+    context "when early-exiting an interaktor with a success" do
+      it "succeeds when the correct attributes are provided" do
+        interaktor.class_eval { success :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          success!(bar: "baz")
+        end
+
+        result = interaktor.call!
+
+        expect(result.success?).to be true
+        expect(result.bar).to eq "baz"
+      end
+
+      it "raises an exception when the correct attributes are not provided" do
+        interaktor.class_eval { success :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          success!({})
+        end
+
+        expect { interaktor.call! }.to raise_error(RuntimeError, /Missing success attrs/)
+      end
+    end
+
+    context "when exiting an interaktor with a failure" do
+      it "fails successfully when the correct attributes are provided" do
+        interaktor.class_eval { failure :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          fail!(bar: "baz")
+        end
+
+        expect { interaktor.call! }.to raise_error(Interaktor::Failure)
+      end
+
+      it "raises an exception when the correct attributes are not provided" do
+        interaktor.class_eval { failure :bar }
+
+        expect(interaktor).to receive(:new).once.with({}).and_call_original
+
+        interaktor.define_method(:call) do
+          fail!({})
+        end
+
+        expect { interaktor.call! }.to raise_error(RuntimeError, /Missing failure attrs/)
+      end
     end
   end
 
