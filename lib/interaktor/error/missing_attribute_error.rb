b68067ff39e01a556e7c101d6085bd9b2b91177c
Taylor Thurlow <thurlow@hey.com>
2021-06-30 17:09:32 -0700

    Implement dry-schema for input, success, and failure attributes

--- lib/interaktor/error/missing_attribute_error.rb
+++
@@ -1,5 +0,0 @@
-class Interaktor::Error::MissingAttributeError < Interaktor::Error::AttributeError
-  def message
-    "Missing attribute(s) in call to #{interaktor} interaktor: #{attributes.join(", ")}"
-  end
-end
