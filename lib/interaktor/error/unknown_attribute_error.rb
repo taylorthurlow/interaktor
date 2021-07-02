3ecc8403f34c1574cbe199041690a87a17ab1d04
Taylor Thurlow <thurlow@hey.com>
2021-07-02 09:43:41 -0700

    Remove unused error classes

--- lib/interaktor/error/unknown_attribute_error.rb
+++
@@ -1,5 +0,0 @@
-class Interaktor::Error::UnknownAttributeError < Interaktor::Error::AttributeError
-  def message
-    "Unknown attribute(s) in call to #{interaktor}: #{attributes.join(", ")}"
-  end
-end
--- lib/interaktor/error/unknown_option_error.rb
+++
@@ -1,5 +0,0 @@
-class Interaktor::Error::UnknownOptionError < Interaktor::Error::OptionError
-  def message
-    "Unknown option(s) in #{interaktor} interaktor: #{options.keys.join(", ")}"
-  end
-end
